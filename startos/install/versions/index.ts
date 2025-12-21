0ef1b9098853ebe36004e99c28549a15ef282865
Matt Hill <mattnine@protonmail.com>
2025-12-21 10:47:52 -0700

    use beta tag

--- startos/install/versions/index.ts
+++ startos/install/versions/index.ts
@@ -1,2 +1,2 @@
-export { v_0_4_0_0 as current } from './v0.4.0.0'
+export { v_0_4_0_0_b0 as current } from './v0.4.0.0.b0'
 export const other = []
--- startos/install/versions/v0.4.0.0.b0.ts
+++ startos/install/versions/v0.4.0.0.b0.ts
@@ -1,7 +1,7 @@
 import { VersionInfo } from '@start9labs/start-sdk'
 
-export const v_0_4_0_0 = VersionInfo.of({
-  version: '0.4.0:0',
+export const v_0_4_0_0_b0 = VersionInfo.of({
+  version: '0.4.0:0-beta.0',
   releaseNotes: 'Revamped for StartOS 0.4.0',
   migrations: {
     up: async ({ effects }) => {},
