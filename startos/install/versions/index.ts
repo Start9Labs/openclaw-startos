6ff72da9ae98b313e5a41f109f65e7fa0051fbf5
Matt Hill <mattnine@protonmail.com>
2026-02-03 08:01:21 -0700

    switch to alpha

--- startos/install/versions/index.ts
+++ startos/install/versions/index.ts
@@ -1,2 +1,2 @@
-export { v_2026_1_29_0_b0 as current } from './v2026.1.29.0.b0'
+export { v_2026_1_29_0_a0 as current } from './v2026.1.29.0.a0'
 export const other = []
--- startos/install/versions/v2026.1.29.0.a0.ts
+++ startos/install/versions/v2026.1.29.0.a0.ts
@@ -1,7 +1,7 @@
 import { VersionInfo } from '@start9labs/start-sdk'
 
-export const v_2026_1_29_0_b0 = VersionInfo.of({
-  version: '2026.1.29:0-beta.0',
+export const v_2026_1_29_0_a0 = VersionInfo.of({
+  version: '2026.1.29:0-alpha.0',
   releaseNotes: {
     en_US: 'Initial release of OpenClaw for StartOS 0.4.0',
     es_ES: 'Lanzamiento inicial de OpenClaw para StartOS 0.4.0',
